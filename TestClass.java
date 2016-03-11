2a544dc4e8ac0a4ed4b218b6198adae3601f342e
MadhavM9 <madhav.munagapati2@gmail.com>
2016-03-11 07:19:08 -0600

    Java Testing

---
+++ TestClass.java
@@ -0,0 +1,24 @@
+
+public class TestClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		
+		
+		System.out.println("Hello World");
+		
+		TestClass test = new TestClass();
+		test.testFun();
+
+	}
+		public void testFun()
+	{
+	 System.out.println("Test World");	
+	}
+			
+			
+	
+	
+
+}
